d475c0211dfa4fc5f52a2307353eb11b889cc5b8
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-03-03 19:06:54 +0500

    fixed LogWindowSource class, added queue for messages

--- src/main/java/log/LogWindowSource.java
+++ src/main/java/log/LogWindowSource.java
@@ -1,7 +1,7 @@
 package log;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+import java.lang.System;
 
 /**
  * Что починить:
@@ -16,14 +16,14 @@ public class LogWindowSource
 {
     private int m_iQueueLength;
     
-    private ArrayList<LogEntry> m_messages;
+    private final Queue<LogEntry> m_messages;
     private final ArrayList<LogChangeListener> m_listeners;
     private volatile LogChangeListener[] m_activeListeners;
     
     public LogWindowSource(int iQueueLength) 
     {
         m_iQueueLength = iQueueLength;
-        m_messages = new ArrayList<LogEntry>(iQueueLength);
+        m_messages = new ArrayDeque<LogEntry>(m_iQueueLength);
         m_listeners = new ArrayList<LogChangeListener>();
     }
     
@@ -48,21 +48,21 @@ public class LogWindowSource
     public void append(LogLevel logLevel, String strMessage)
     {
         LogEntry entry = new LogEntry(logLevel, strMessage);
-        m_messages.add(entry);
-        LogChangeListener [] activeListeners = m_activeListeners;
+        synchronized (m_messages) {
+            if (m_messages.size() == m_iQueueLength)
+                m_messages.poll();
+            m_messages.offer(entry);
+        }
+        LogChangeListener[] activeListeners = m_activeListeners;
         if (activeListeners == null)
         {
             synchronized (m_listeners)
             {
-                if (m_activeListeners == null)
-                {
-                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
-                    m_activeListeners = activeListeners;
-                }
+                activeListeners = m_listeners.toArray(new LogChangeListener[0]);
+                m_activeListeners = activeListeners;
             }
         }
-        for (LogChangeListener listener : activeListeners)
-        {
+        for (LogChangeListener listener : activeListeners) {
             listener.onLogChanged();
         }
     }
@@ -74,12 +74,13 @@ public class LogWindowSource
 
     public Iterable<LogEntry> range(int startFrom, int count)
     {
-        if (startFrom < 0 || startFrom >= m_messages.size())
+        var messages = new ArrayList<LogEntry>(m_messages);
+        if (startFrom < 0 || startFrom >= messages.size())
         {
             return Collections.emptyList();
         }
         int indexTo = Math.min(startFrom + count, m_messages.size());
-        return m_messages.subList(startFrom, indexTo);
+        return messages.subList(startFrom, indexTo);
     }
 
     public Iterable<LogEntry> all()
