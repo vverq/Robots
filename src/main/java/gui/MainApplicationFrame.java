5dac1fc83ef601def98ca6dce283e4c858fa5065
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-03-15 13:00:59 +0500

    added using ResourceBundle, added new MenuItem for choosing language, fixed robot`s movement

--- src/main/java/gui/MainApplicationFrame.java
+++ src/main/java/gui/MainApplicationFrame.java
@@ -5,6 +5,9 @@ import log.Logger;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -20,6 +23,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
+    private Locale locale = Locale.getDefault();
+    private ResourceBundle bundle = ResourceBundle.getBundle("MainApplicationFrameBundle", locale);
     public MainApplicationFrame() {
         //Make the big window be indented 50 pixels from each edge
         //of the screen.
@@ -30,7 +35,6 @@ public class MainApplicationFrame extends JFrame
 //            screenSize.height - inset*2);
         setLocationRelativeTo(null);
         setContentPane(desktopPane);
-
         addWindow(createLogWindow());
         addWindow(createGameWindow());
         setJMenuBar(generateMenuBar());
@@ -39,15 +43,18 @@ public class MainApplicationFrame extends JFrame
     
     protected LogWindow createLogWindow()
     {
-        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
+        var logWindowTitle = bundle.getString("logWindowTitle");
+        var startLogMessage = bundle.getString("startLogMessage");
+        LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource(), logWindowTitle);
         logWindow.setLocation(10,10);
-        Logger.debug("Протокол работает");
+        Logger.debug(startLogMessage);
         return logWindow;
     }
 
     protected GameWindow createGameWindow()
     {
-        GameWindow gameWindow = new GameWindow();
+        var gameWindowTitle = bundle.getString("gameWindowTitle");
+        GameWindow gameWindow = new GameWindow(gameWindowTitle);
         gameWindow.setSize(400, 400);
 //        gameWindow.setAlignmentX(GameWindow.CENTER_ALIGNMENT);
         return gameWindow;
@@ -93,38 +100,76 @@ public class MainApplicationFrame extends JFrame
         JMenuBar menuBar = new JMenuBar();
         JMenu visualModeMenu = CreateVisualModeMenu();
         JMenu testMenu = CreateTestMenu();
+        JMenu languageMenu = CreateLanguageMenu();
         menuBar.add(visualModeMenu);
         menuBar.add(testMenu);
+        menuBar.add(languageMenu);
         return menuBar;
+    }
+
+    private JMenu CreateLanguageMenu()
+    {
+        var languageMenuTitle = bundle.getString("languageMenuTitle");
+        var languageMenuDescription = bundle.getString("testMenuDescription");
+        var russianMenuItemTitle = bundle.getString("russianMenuItemTitle");
+        var englishMenuItemTitle = bundle.getString("englishMenuItemTitle");
+        JMenu languageMenu = new JMenu(languageMenuTitle);
+        languageMenu.setMnemonic(KeyEvent.VK_T);
+        languageMenu.getAccessibleContext()
+                .setAccessibleDescription(languageMenuDescription);
+        JMenuItem russianItem = new JMenuItem(russianMenuItemTitle, KeyEvent.VK_S);
+        russianItem.addActionListener((event) -> {
+            locale = Locale.getDefault();
+        });
+        languageMenu.add(russianItem);
+        JMenuItem englishItem = new JMenuItem(englishMenuItemTitle, KeyEvent.VK_S);
+        englishItem.addActionListener((event) -> {
+            locale = Locale.ENGLISH;
+        });
+        languageMenu.add(englishItem);
+        return languageMenu;
     }
 
     private JMenu CreateTestMenu()
     {
-        JMenu testMenu = new JMenu("Тесты");
+        var testMenuTitle = bundle.getString("testMenuTitle");
+        var testMenuDescription = bundle.getString("testMenuDescription");
+        var logMessageItemText = bundle.getString("logMessageItemText");
+        var newLogMessage = bundle.getString("newLogMessage");
+        JMenu testMenu = new JMenu(testMenuTitle);
         testMenu.setMnemonic(KeyEvent.VK_T);
         testMenu.getAccessibleContext()
-                .setAccessibleDescription("Тестовые команды");
-        JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
+                .setAccessibleDescription(testMenuDescription);
+        JMenuItem addLogMessageItem = new JMenuItem(logMessageItemText, KeyEvent.VK_S);
         addLogMessageItem.addActionListener((event) -> {
-            Logger.debug("Новая строка");
+            Logger.debug(newLogMessage);
         });
         testMenu.add(addLogMessageItem);
         return testMenu;
     }
 
+    private void UpdateNames()
+    {
+        //обновить названия в меню и окнах соответственно текущей локали
+    }
+
     private JMenu CreateVisualModeMenu()
     {
-        JMenu visualModeMenu = new JMenu("Режим отображения");
+        var visualModeMenuTitle = bundle.getString("visualModeMenuTitle");
+        var visualModeMenuDescription = bundle.getString("visualModeMenuDescription");
+        var systemModeText = bundle.getString("systemModeText");
+        var crossplatformModeText = bundle.getString("crossplatformModeText");
+        JMenu visualModeMenu = new JMenu(visualModeMenuTitle);
         visualModeMenu.setMnemonic(KeyEvent.VK_V);
         visualModeMenu
                 .getAccessibleContext()
-                .setAccessibleDescription("Управление режимом отображения приложения");
-        JMenuItem systemMode = new JMenuItem("Системная схема", KeyEvent.VK_S);
+                .setAccessibleDescription(visualModeMenuDescription);
+        JMenuItem systemMode = new JMenuItem(systemModeText, KeyEvent.VK_S);
         systemMode.addActionListener((event) -> {
             setVisualMode(UIManager.getSystemLookAndFeelClassName());
             this.invalidate();
         });
-        JMenuItem crossplatformMode = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
+        JMenuItem crossplatformMode = new JMenuItem(crossplatformModeText, KeyEvent.VK_S);
         crossplatformMode.addActionListener((event) -> {
             setVisualMode(UIManager.getCrossPlatformLookAndFeelClassName());
             this.invalidate();
