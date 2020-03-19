b61020095b2c2b8d4fe4fbc882d02d891708f956
inS&&M <18631869w@gmail.com>
2020-03-19 23:55:39 +0500

    added ExitWindow class, fixed exitDialog

--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -2,13 +2,13 @@ package gui;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class GameWindow extends JInternalFrame
 {
     private final GameVisualizer m_visualizer;
-    public GameWindow(String title)
+
+    GameWindow(String title)
     {
         super(title, true, true, true, true);
         m_visualizer = new GameVisualizer();
@@ -16,6 +16,12 @@ public class GameWindow extends JInternalFrame
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         setDefaultLocale(JInternalFrame.getDefaultLocale());
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         pack();
+    }
+
+    public void dispose()
+    {
+        setVisible(false);
     }
 }
--- src/main/java/gui/LogWindow.java
+++ src/main/java/gui/LogWindow.java
@@ -16,7 +16,7 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
     private LogWindowSource m_logSource;
     private TextArea m_logContent;
 
-    public LogWindow(LogWindowSource logSource, String title)
+    LogWindow(LogWindowSource logSource, String title)
     {
         super(title, true, true, true, true);
         m_logSource = logSource;
